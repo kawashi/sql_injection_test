e504a9682baa54346369b9880f4dea99fdaf49ce
Marihuana <aisatu_nomahou@yahoo.co.jp>
2016-06-02 14:46:01 +0900

    ログイン機能と新規登録機能を実装した

--- index.php
+++ index.php
@@ -1,49 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<?php
-    if( isset($_POST['submit']) ){
-        // DB接続・選択
-        $link = mysql_connect('localhost', 'kawashi', 'yakisobameron');
-        mysql_select_db('fjb', $link);
-        $result = mysql_query("SELECT * FROM users WHERE users = '".$_POST['name']."'");
-        while( $row = mysql_fetch_array($result) ){
-            if( $_POST["pass"] == $row ){
-                $_COOKIE["ok"] = 1;
-                header("Location: http://google.com");
-            }
-        }
-    }
-?>
-
-
 <head>
     <meta charset="UTF-8">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+    <title>SQL Injection - Test Server</title>
+    <?php
+    /* サインイン */
+    if( isset($_POST['signin']) ){
+        // DB接続・選択 TODO:要リファクタ
+        $link = mysql_connect('mysql.hostinger.jp', 'u554433119_fjb', 'password');
+        if(!$link) exit(mysql_error()."コネクトできてないやつ");
+        if(!mysql_select_db('u554433119_fjb', $link)) exit(mysql_error()."選択できてないやつ"); 
+        
+        // SQL発行
+        $query  = "SELECT * FROM users WHERE name = '".$_POST['name']."'";
+        echo '<script>alert("'.$query.'");</script>';
+        $result = mysql_query($query);
+        if(!$result) exit(mysql_error()."クエリ失敗奴");
+        
+        // 結果照合
+        while( $row = mysql_fetch_array($result) ){
+            if( $_POST["pass"] == $row["pass"] ){
+                setcookie('login', 'true', time() + 3600);
+                header("Location: show.php");
+                exit;
+            }
+        }
+    }
+    /* サインアップ */
+    if( isset($_POST['signup']) ){
+        // DB接続・選択 TODO:要リファクタ
+        $link = mysql_connect('mysql.hostinger.jp', 'u554433119_fjb', 'password');
+        if(!$link) exit(mysql_error()."コネクトできてないやつ");
+        if(!mysql_select_db('u554433119_fjb', $link)) exit(mysql_error()."選択できてないやつ"); 
+        
+        // SQL発行
+        $query  = "INSERT INTO users VALUES(null, '". $_POST["name"] ."', '". $_POST["pass"] ."')";
+        echo '<script>alert("'.$query.'");</script>';
+        $result = mysql_query($query);
+        if(!$result) exit(mysql_error()."クエリ失敗奴");
+        else         header("Location: index.php?flag=success");
+    }
+    
+    ?>
 </head>
 <body>
     <header>
         <div class="text-center"><h1>Hello, Kawashi!</h1></div>
     </header>
     
-    <div class="container text-center main">
-        <h1>Please Login.</h1>
-        <div class="row">
-            <form action="index.php" method="post" >
-               <div>
-                    <input type="text" name="name" class="form-control" placeholder="ユーザ名"/>
-                </div>
-                <div>
-                    <input type="text" name="name" class="form-control" placeholder="パスワード"/>
-                </div>
-                <div>
-                    <input type="submit" class="btn btn-primary btn-block" value="submit!" name="submit">
-                </div>
-            </form>
+    <div class="container text-center">
+        <div class="row main">
+           <?php if($_GET["flag"] == "success"){
+                echo '<div class="bg-success text-success success-message"><p>Success, Please Login!</p></div>';
+            } ?>
+            <div class="signin col-md-6">
+                <h1>Login Form.</h1>
+                <form action="index.php" method="post" >
+                   <div>
+                        <input type="text" name="name" class="form-control" placeholder="username"/>
+                    </div>
+                    <div>
+                        <input type="password" name="pass" class="form-control" placeholder="password"/>
+                    </div>
+                    <div>
+                        <input type="submit" class="btn btn-default btn-block" value="signin" name="signin">
+                    </div>
+                </form>
+            </div>
+            <div class="signup col-md-6">
+                <h1>Create Account.</h1>
+                <form action="index.php" method="post">
+                    <div>
+                        <input type="text" name="name" class="form-control" placeholder="username">
+                    </div>
+                    <div>
+                        <input type="password" name="pass" class="form-control" placeholder="password">
+                    </div>
+                    <div>
+                        <input type="submit" class="btn btn-primary btn-block" name="signup" value="signup">
+                    </div>
+                </form>
+            </div> 
+            <hr>
         </div>
-        <hr>
         <div class="message">
             <h1 class="ok">やってもゆるされること(自己責任)</h1>
             <ul class="list-unstyled">
---
+++ show.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="show.css">
+    <title>MainPage</title>
+    <?php
+        // ログインしてなかったら弾き出す
+        if(empty($_COOKIE["login"]) || $_COOKIE["login"] != "true"){
+            header("Location: index.php");
+            exit;
+        }
+    
+        // ログアウト処理
+        if(isset($_POST["logout"])){
+            setcookie('login', 'false', time()-1800);
+            header("Location: index.php");
+            exit;
+        }
+    ?>
+</head>
+<body>
+    <div class="container">
+        <header class="text-center"><h1>Hello, Kawashi!</h1></header>
+        <div class="main text-center">
+            <h1>ログイン成功</h1>
+            <form action="show.php" method="post">
+                <input type="submit" name="logout" value="Logout" class="btn btn-default">
+            </form>
+        </div>
+    </div>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
